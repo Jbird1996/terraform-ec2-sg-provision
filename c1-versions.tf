1a2d7d622724737bda9b00bde0a3ae9cc5adfe20
Jordan <jordan2708@hotmail.co.uk>
2023-01-16 12:41:25 +0000

    committing vpc files

---
+++ c1-versions.tf
@@ -0,0 +1,18 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }        
+  }
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
