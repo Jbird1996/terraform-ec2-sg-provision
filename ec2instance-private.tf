5e9c4b564ca214e15f674c543bf492ce98feebf4
Jordan <jordan2708@hotmail.co.uk>
2023-01-17 12:40:33 +0000

    updating to complete 3 tier architecture

--- ec2instance-private.tf
+++ ec2instance-private.tf
@@ -3,14 +3,19 @@
 module "ec2_private" {
   depends_on = [ module.vpc ]
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "4.3.0"
+  version = "2.17.0"
 
   name                   = "${var.environment}-vm"
   ami                    = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
   vpc_security_group_ids = [module.private_bastion_sg.security_group_id]
-  subnet_id = element(module.vpc.private_subnets, 0)
+  subnet_ids             = [
+    module.vpc.private_subnets[0],
+    module.vpc.private_subnets[1]
+  ]
+   
+  instance_count         = var.private_instance_count
   user_data = file("${path.module}/app1-install.sh")
   tags = local.common_tags
 }
